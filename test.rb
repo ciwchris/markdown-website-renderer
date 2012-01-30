77d581ea2890a417d71729aa0937d671b3d35b82
Christopher Lopes <christopher.lopes@gmail.com>
2012-01-29 21:17:42 -0800

    Working rendering markdown files one directory down

--- test.rb
+++ test.rb
@@ -2,10 +2,22 @@ require 'rubygems'
 require 'redcarpet'
 require 'git'
 
+renderer = Redcarpet::Render::HTML.new(:no_links => true, :hard_wrap => true)
+markdown = Redcarpet::Markdown.new(renderer)
+
 g = Git.open('.')
+tree = g.gtree('master')
+tree.files.each do |f|
+	if (f.first.end_with?('.md'))
+		puts markdown.render(g.cat_file(f[1]))
+	end
+end
 
-puts g.index
-
-#renderer = Redcarpet::Render::HTML.new(:no_links => true, :hard_wrap => true)
-#markdown = Redcarpet::Markdown.new(renderer)
-#puts markdown.render("text")
+tree.subdirectories.each do |d|
+	subtree = g.gtree(d[1])
+	subtree.files.each do |f|
+		if (f.first.end_with?('.md'))
+			puts markdown.render(g.cat_file(f[1]))
+		end
+	end
+end

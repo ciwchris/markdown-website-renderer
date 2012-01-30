424201f22f3a09fe4d01e8eef1fc37ef96e2e913
Christopher Lopes <christopher.lopes@gmail.com>
2012-01-29 23:37:59 -0800

    Webserver portion is now rendering all of the markdown content as html

--- app.rb
+++ app.rb
@@ -1,8 +1,43 @@
 require 'rubygems'
 require 'sinatra'
 require 'redcarpet'
+require 'git'
+
+class MarkdownRenderer
+	def initialize ()
+		@content = []
+		renderer = Redcarpet::Render::HTML.new(:no_links => true, :hard_wrap => true)
+		@markdown = Redcarpet::Markdown.new(renderer)
+		@g = Git.open('.')
+	end
+
+	def start_search
+		tree = @g.gtree('Setup')
+		render_markdown_files(tree)
+		search_directories(tree)
+		@content
+	end
+
+	private
+
+	def render_markdown_files(tree)
+		tree.files.keys.each do |f|
+			if (f.end_with?('.md'))
+				@content.push(@markdown.render(@g.cat_file(tree.files[f].to_s)))
+			end
+		end
+	end
+
+	def search_directories(tree)
+		return if tree == nil
+		tree.subdirectories.keys.each do |d|
+			subtree = @g.gtree(tree.subdirectories[d])
+			render_markdown_files(subtree)
+			search_directories(subtree)
+		end
+	end
+end
 
 get '/' do
-	markdown = Redcarpet.new("Hello World!")
-	puts markdown.to_html
+	MarkdownRenderer.new.start_search
 end

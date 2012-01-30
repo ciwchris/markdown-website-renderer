04526549b3b8c1be663306aaa28ce2872a889555
Christopher Lopes <christopher.lopes@gmail.com>
2012-01-29 21:46:25 -0800

    Structure the code a littel into a class. Recursively check subdirectories for markdown files. Add a third layer subdirectory for testing

--- test.rb
+++ test.rb
@@ -2,22 +2,37 @@ require 'rubygems'
 require 'redcarpet'
 require 'git'
 
-renderer = Redcarpet::Render::HTML.new(:no_links => true, :hard_wrap => true)
-markdown = Redcarpet::Markdown.new(renderer)
-
-g = Git.open('.')
-tree = g.gtree('master')
-tree.files.each do |f|
-	if (f.first.end_with?('.md'))
-		puts markdown.render(g.cat_file(f[1]))
+class MarkdownRenderer
+	def initialize ()
+		renderer = Redcarpet::Render::HTML.new(:no_links => true, :hard_wrap => true)
+		@markdown = Redcarpet::Markdown.new(renderer)
+		@g = Git.open('.')
 	end
-end
 
-tree.subdirectories.each do |d|
-	subtree = g.gtree(d[1])
-	subtree.files.each do |f|
-		if (f.first.end_with?('.md'))
-			puts markdown.render(g.cat_file(f[1]))
+	def start_search
+		tree = @g.gtree('Setup')
+		render_markdown_files(tree)
+		search_directories(tree)
+	end
+
+	private
+
+	def render_markdown_files(tree)
+		tree.files.each do |f|
+			if (f.first.end_with?('.md'))
+				puts @markdown.render(@g.cat_file(f[1]))
+			end
+		end
+	end
+
+	def search_directories(tree)
+		return if tree == nil
+		tree.subdirectories.each do |d|
+			subtree = @g.gtree(d[1])
+			render_markdown_files(subtree)
+			search_directories(subtree)
 		end
 	end
 end
+
+MarkdownRenderer.new.start_search

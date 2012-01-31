b8300ba4267a651634ff5b5c27d390e5b0c955ba
Christopher Lopes <christopher.lopes@gmail.com>
2012-01-30 17:26:15 -0800

    Now recursively looking through the directory tree searching for markdown files and displaying them.

--- app.rb
+++ app.rb
@@ -1,45 +1,28 @@
 require 'rubygems'
 require 'sinatra'
 require 'redcarpet'
-require 'git'
+require 'find'
 
 class MarkdownRenderer
 	def initialize ()
 		@content = {}
 		renderer = Redcarpet::Render::HTML.new(:no_links => true, :hard_wrap => true)
 		@markdown = Redcarpet::Markdown.new(renderer)
-		@g = Git.open('.')
 	end
 
 	def start_search
-		tree = @g.gtree('master')
-		render_markdown_files(tree)
-		search_directories(tree)
+		Dir['**/*.md'].each {|fileName|
+			name = create_tab_name_from(fileName)
+			file = File.open(fileName)
+			@content[name] = @markdown.render(file.read)
+		}
 		@content
 	end
 
 	private
 
-	def render_markdown_files(tree)
-		tree.files.keys.each do |f|
-			if (f.end_with?('.md'))
-				key = create_key_from(f)
-				@content[key] = @markdown.render(@g.cat_file(tree.files[f].to_s))
-			end
-		end
-	end
-
-	def search_directories(tree)
-		return if tree == nil
-		tree.subdirectories.keys.each do |d|
-			subtree = @g.gtree(tree.subdirectories[d])
-			render_markdown_files(subtree)
-			search_directories(subtree)
-		end
-	end
-
-	def create_key_from(key)
-		key.sub(/\.md$/, '').gsub(/[-_]/, ' ').capitalize
+	def create_tab_name_from(name)
+		File.basename(name, '.md').gsub(/[-_]/, ' ').capitalize
 	end
 end
 
